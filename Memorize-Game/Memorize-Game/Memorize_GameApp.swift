21733d1799b606d40dbba6b39d4562c4272239ae
Ahmed Ali <ahmed.aliamin@vodafone.com>
2023-09-13 15:55:22 +0300

    Init our Memorize game
    the initial commit

---
+++ Memorize-Game/Memorize-Game/Memorize_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_GameApp.swift
+//  Memorize-Game
+//
+//  Created by Ahmed Ali on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
