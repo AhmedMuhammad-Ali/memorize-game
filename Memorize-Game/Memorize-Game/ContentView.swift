9ac4b6722c42b4d842640ab6d58402f135ee614e
Ahmed Ali <ahmed.aliamin@vodafone.com>
2023-09-19 18:50:27 +0300

    add and remove card using new buttons + −

--- Memorize-Game/Memorize-Game/ContentView.swift
+++ Memorize-Game/Memorize-Game/ContentView.swift
@@ -9,14 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     let emojis = ["🤹🏻", "🧜🏻‍♂️", "🐣", "👨🏻‍🎤"]
+    @State var cardCount = 4
+    
     var body: some View {
+        VStack {
+            cards
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    var cards: some View {
         HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
         }
         .foregroundColor(.orange)
-        .padding()
+    }
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+            
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjusters(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }) {
+            Image(systemName: symbol)
+        }.disabled(cardCount + offset > emojis.count || cardCount + offset < 1)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjusters(by: -1, symbol: "minus.circle.fill")
+    }
+    var cardAdder: some View {
+        cardCountAdjusters(by: +1, symbol: "plus.circle.fill")
     }
 }
 
